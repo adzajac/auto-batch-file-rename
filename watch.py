e71ba50ad50e3878f878884e60dc3dde72152cf4
work <adrianzdev@gmail.com>
2019-07-05 22:07:42 +0100

    now can be lunched with args to specify directory to watch, string to be replaced, and string that replaces

--- watch.py
+++ watch.py
@@ -1,17 +1,55 @@
-import sys
+import os, sys
 import time
-import logging
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
+from watchdog.events import FileSystemEventHandler
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    event_handler = LoggingEventHandler()
+import argparse
+
+    
+class FolderChangeHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        num_of_renames = 0
+        for file in os.listdir(args.path):
+            if os.path.abspath(file) == os.path.abspath(__file__):  # skip this very file
+                continue     
+            if os.path.isdir(file):          # skip if it's a folder
+                continue
+            if args.old in file:
+                new_name = file.replace(args.old, args.new)
+                print("renaming: {}  -->  {}".format(file, new_name))
+                os.rename(args.path+'/'+file, args.path+'/'+new_name)
+                num_of_renames += 1
+        if num_of_renames:
+            print("Done. Num of renamed files: {}".format(num_of_renames))
+    
+
+    
+if __name__ == "__main__":    
+    #input arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--path',
+        help='directory path to watch for changes',
+        default ='.'
+    )
+    parser.add_argument(
+        '--old',
+        help='part of the filename to be replaced',
+        required=True
+    )
+    parser.add_argument(
+        '--new',
+        help='what should be put in place of --old',
+        required=True
+    )
+    args = parser.parse_args()
+    
+
+
+    
+    event_handler = FolderChangeHandler()
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
+    observer.schedule(event_handler, args.path, recursive=True)
     observer.start()
     try:
         while True:
